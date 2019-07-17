a592218378ab3a18df3222f159116c03b39e3626
Robert Keen <robertkeen9@gmail.com>
2019-07-17 14:37:33 -0700

    converted everything into a class

--- detection.cpp
+++ detection.cpp
@@ -1,6 +1,9 @@
+#include <stdio.h>
+
 #include "opencv2/opencv.hpp"
 #include "opencv2/xfeatures2d.hpp"
-#include <stdio.h>
+#include "buoyDetector.cpp"
+
 
 // C++ adaptation of this code was done based off the tutorial from
 // https://docs.opencv.org/3.4/d5/d6f/tutorial_feature_flann_matcher.html
@@ -9,63 +12,14 @@
 #define MIN_MATCH_COUNT 10
 
 
-int main(){
-
-    // Define all the Variables
-    cv::Mat jiangshi = cv::imread("images/Jiangshi_lg.png", cv::IMREAD_GRAYSCALE);
-    cv::Ptr<cv::xfeatures2d::SIFT> sift = cv::xfeatures2d::SIFT::create();
-    std::vector<cv::KeyPoint> keypoints1, keypoints2;
-    cv::Mat descriptors1, descriptors2;
-    cv::Ptr<cv::DescriptorMatcher> matcher = cv::DescriptorMatcher::create(cv::DescriptorMatcher::FLANNBASED);
-    sift->detectAndCompute(jiangshi, cv::noArray(), keypoints1, descriptors1);
+int main(){ 
 
     // capture video from webcam
-    cv::VideoCapture cap(0);
-    cv::Mat frame;
-    cv::Mat gray_frame;
+    cv::VideoCapture cap(0); 
+    BuoyDetector detector(Jiangshi, cap);
     while(1){
-        cap >> frame;
-
-        cv::cvtColor(frame, gray_frame, cv::COLOR_BGR2GRAY);
-        sift->detectAndCompute(gray_frame, cv::noArray(), keypoints2, descriptors2);
-        // matching the descriptor vectors with a FLANN based matcher
-        std::vector< std::vector<cv::DMatch> > knn_matches;
-        matcher->knnMatch( descriptors1, descriptors2, knn_matches, 2);
-
-        // Filter matches using the Lowe's ratio test
-        const float ratio_thresh = 0.6f;
-        std::vector<cv::DMatch> good_matches;
-        for (size_t i = 0; i < knn_matches.size(); i++){
-            if ( knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance){
-                good_matches.push_back(knn_matches[i][0]);
-            }
-        }
-        cv::Mat img_matches;
-        if( good_matches.size() >= MIN_MATCH_COUNT){
-            std::vector<cv::Point2f> src_pts;
-            std::vector<cv::Point2f> dst_pts;
-
-            for( cv::DMatch x : good_matches){
-                src_pts.push_back(keypoints1[x.queryIdx].pt);
-                dst_pts.push_back(keypoints2[x.trainIdx].pt);
-            }
-            cv::Mat H = cv::findHomography(src_pts, dst_pts, cv::RANSAC);
-            // get the corners from the jiangshi image
-            std::vector<cv::Point2f> obj_corners(4);
-            obj_corners[0] = cvPoint(0,0); obj_corners[1] = cvPoint( jiangshi.cols, 0 );
-            obj_corners[2] = cvPoint( jiangshi.cols, jiangshi.rows ); obj_corners[3] = cvPoint( 0, jiangshi.rows );
-            std::vector<cv::Point2f> scene_corners(4);
-            
-            if ( !H.empty() ){
-                cv::perspectiveTransform( obj_corners, scene_corners, H);
-                cv::Rect rect(scene_corners[0], scene_corners[2]);
-                cv::rectangle(frame, rect, cv::Scalar(0,0,255), 3);
-            }
-
-        }
-        cv::imshow("frame", frame);
+        detector.Demo();
         if(cv::waitKey(30) >= 0) break;
-
     }
     cap.release();
     cv::destroyAllWindows();
